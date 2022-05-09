cdd93c4f61b3e74a0ab3a7e144738709737f3b9f
JosephStalin <Berkeyboy1@gmail.com>
2022-05-09 12:51:59 -0400

    names

--- src/components/StoryModal/StoryModal.jsx
+++ src/components/StoryModal/StoryModal.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './styles.css';
 
 import {
   Modal,
@@ -11,13 +12,14 @@ import {
 
 const StoryModal = (props) => {
   const { closeModal, show, story } = props;
+  const fullscreen = true;
 
   return (
-    <Modal show={show} onClick={closeModal} style={{ opacity: 1 }}>
-      <ModalHeader>
+    <Modal show={show} onClick={closeModal} style={{ opacity: 1 }} dialogClassName='yea' fullscreen={fullscreen}>
+      <ModalHeader >
         <ModalTitle>{story.title}</ModalTitle>
       </ModalHeader>
-      <ModalBody>
+      <ModalBody className="body">
         <iframe src={story.url} title={story.title}></iframe>
       </ModalBody>
       <ModalFooter>
